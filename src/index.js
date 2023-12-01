dee429b6f1cd02bbec0baf71d2f67f6265f7dd1d
Developer1 <developer1@Developer1s-MacBook-Pro.local>
2023-12-01 07:18:57 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const  express  = require("express");
 const app=express();
 app.get('/',(req,res)=>{
-    res.status(200).send('<h1>Its working iyyy </h1>')
+    res.status(200).send('<h1>Its working  343</h1>')
 })
-let port =process.env.PORT || 4000;
+let port =process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log('listening on port.',port)
 })
